d68652ea8a0c42453d2f1b51631c24fa667d389c
Rachit Srivastava <rachitsrivastava77@gmail.com>
2020-11-21 21:59:10 +0530

    Azure Function+Storage Queue

---
+++ OnPaymentReceived/OnPaymentReceived/Function1.cs
@@ -0,0 +1,54 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+//Add Nuget --> microsoft.azure.webjobs.storage for Queue
+namespace OnPaymentReceived
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            [Queue("orders")] IAsyncCollector<Order> orderQueue, //It creates the Queue even if it does not exists
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            //string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            //dynamic data = JsonConvert.DeserializeObject(requestBody);
+
+            var order = JsonConvert.DeserializeObject<Order>(requestBody);
+            //name = name ?? order?.name;
+
+            //Pass the order to the queue
+            await orderQueue.AddAsync(order);
+            log.LogInformation($"Order {order.OrderId} received from {order.Email} for product {order.ProductId} " );
+
+            //string responseMessage = string.IsNullOrEmpty(name)
+            //    ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+            //    : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult("Thank you for placing order!!");
+        }
+    }
+
+    public class Order
+    {
+        public string OrderId { get; set; }
+
+        public string ProductId { get; set; }
+
+        public string Email { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
---
+++ OnPaymentReceived/Readme.txt
@@ -0,0 +1,1 @@
+Simple WebHook that accepts Order details sent to us by a payment provider and we wrote a JSON message onto the Storage Queue whenever that function was called.
