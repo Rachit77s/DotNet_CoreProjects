0b351530cb32b00409585db7403a14f1eeba0bb7
Rachit Srivastava <rachitsrivastava77@gmail.com>
2020-11-21 21:53:43 +0530

    CRUD Operations

---
+++ CRUDDBFirst/CRUDDBFirst/Controllers/StudentController.cs
@@ -0,0 +1,49 @@
+﻿using CRUDDBFirst.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUDDBFirst.Controllers
+{
+    public class StudentController : Controller
+    {
+        //This name derived from Connection String in Web.config
+        CrudOperationsEntities dbObj = new CrudOperationsEntities();
+
+        // GET: Student
+        public ActionResult Student()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult AddStudent(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                //Student class
+                Student studentObj = new Student();
+                studentObj.Name = student.Name;
+                studentObj.Fname = student.Fname;
+                studentObj.Email = student.Email;
+                studentObj.Mobile = student.Mobile;
+                studentObj.Description = student.Description;
+
+                dbObj.Students.Add(studentObj);
+                dbObj.SaveChanges();
+            }
+
+            ModelState.Clear();
+            return View("Student");
+        }
+
+        public ActionResult StudentList()
+        {
+            var list = dbObj.Students.ToList();
+            return View(list);
+        }
+    }
+}
---
+++ CoreCrud/CoreCrud/Models/EmployeeContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreCrud.Models
+{
+    public class EmployeeContext: DbContext
+    {
+        //Constructor gets initialised by Dependency Injection
+        public EmployeeContext(DbContextOptions<EmployeeContext> options):base(options)
+        {
+
+        }
+
+        //Add Employee class to DBModel
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
