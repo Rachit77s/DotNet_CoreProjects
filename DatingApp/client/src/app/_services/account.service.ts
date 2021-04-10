7dfb2695a4c3409bac1f210c11429a34ac84c925
Rachit Srivastava <rachitsrivastava77@gmail.com>
2021-04-10 23:58:50 +0530

    Added Registeration Form

--- DatingApp/client/src/app/_services/account.service.ts
+++ DatingApp/client/src/app/_services/account.service.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +11,39 @@ import { Injectable } from '@angular/core';
 export class AccountService {
   baseUrl = 'https://localhost:44347/api/';
 
+  //Create a observable where we will store the current user, its like a buffer object ReplaySubject
+  //Store only 1 previous value i.e. Store only one user
+  private currentUserSource = new ReplaySubject<User>(1);
+  
+  //It is a observable
+  currentUser$ = this.currentUserSource.asObservable();   
+
   constructor(private http : HttpClient) { 
 
   }
 
   login(model: any) {
-    return this.http.post(this.baseUrl + 'account/login', model);
+    return this.http.post(this.baseUrl + 'account/login', model).pipe(
+      map((response : User) => {
+        const user = response;
+        if(user) {
+          //When the user login save it in local storage
+          localStorage.setItem('user', JSON.stringify(user));
+          //Set the current user
+          this.currentUserSource.next(user);
+        }
+
+      })
+    );
+  }
+
+  //Helper method
+setCurrentUser(user: User) {
+  this.currentUserSource.next(user);
+}
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null);
   }
 }
--- DatingApp/client/src/app/nav/nav.component.ts
+++ DatingApp/client/src/app/nav/nav.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
+
 
 @Component({
   selector: 'app-nav',
@@ -8,20 +11,31 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
   model: any = {}
-  loggedIn: boolean;
 
-  constructor(private accountService: AccountService) { }
+
+  constructor(public accountService: AccountService) { }
 
   ngOnInit(): void {
+
   }
 
   login() {
     this.accountService.login(this.model).subscribe(response => {
       console.log(response);
-      this.loggedIn = true;
     }, error => {
     console.log(error);
     });
   }
 
+  logout(){
+    this.accountService.logout();
+  }
+
+  // getCurrentUser() {
+  //   this.accountService.currentUser$.subscribe(user => {
+  //     this.loggedIn = !!user; //This means if the user is null then set it to false else set to true
+  //   }, error => {
+  //     console.log(console.error);    
+  //   })
+  // }
 }
