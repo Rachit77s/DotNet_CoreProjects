aed29ed32268da0e9eddecede8c34303468c822c
Rachit Srivastava <rachitsrivastava77@gmail.com>
2021-04-11 16:33:29 +0530

    Added Registration on UI

--- DatingApp/client/src/app/register/register.component.ts
+++ DatingApp/client/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+
 
 @Component({
   selector: 'app-register',
@@ -7,19 +9,26 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
   @Input() usersFromHomeComponent: any;
+  @Output() cancelRegister = new EventEmitter();
   model: any = {};
   
-  constructor() { }
+  constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
   }
 
   register() {
-    console.log(this.model);
+    this.accountService.register(this.model).subscribe(response => {
+      console.log(response);
+      this.cancel();
+    }, error => {
+      console.error(error);
+    } 
+    )
   }
 
   cancel() {
-
+    this.cancelRegister.emit(false);
   }
 
 }
