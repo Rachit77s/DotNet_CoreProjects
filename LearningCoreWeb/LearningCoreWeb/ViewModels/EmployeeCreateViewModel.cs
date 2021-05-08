fafef620b45ad3c2613747266a4ad6249ae4cae1
Rachit Srivastava <rachitsrivastava77@gmail.com>
2021-05-08 19:02:06 +0530

    Added File Upload

---
+++ LearningCoreWeb/LearningCoreWeb/ViewModels/EmployeeCreateViewModel.cs
@@ -0,0 +1,24 @@
+﻿using LearningCoreWeb.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearningCoreWeb.ViewModels
+{
+    public class EmployeeCreateViewModel
+    {
+        [Required(ErrorMessage = "Please provide a value for Name field")]
+        public string Name { get; set; }
+
+        [Display(Name = "Office Email")]
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$",
+        ErrorMessage = "Invalid email format")]
+        [Required(ErrorMessage = "Please provide a value for Email field")]
+        public string Email { get; set; }
+        public DepartmentEnum Department { get; set; }
+        public IFormFile Photo { get; set; }
+    }
+}
---
+++ LearningCoreWeb/LearningCoreWeb/ViewModels/HomeDetailsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using LearningCoreWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearningCoreWeb.ViewModels
+{
+    public class HomeDetailsViewModel
+    {
+        public Employee Employee { get; set; }
+    }
+}
