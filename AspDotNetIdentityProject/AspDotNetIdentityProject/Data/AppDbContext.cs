23fd2048c8dc06e751f803b4f3e9b9c35cad4b1c
Rachit Srivastava <rachitsrivastava77@gmail.com>
2021-05-13 15:35:28 +0530

    Added ASP.NET Identity Functionality 1

---
+++ AspDotNetIdentityProject/AspDotNetIdentityProject/Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspDotNetIdentityProject.Data
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Rachit: Add this otherwise we would get exception during migration for IdentityDbContext
+            //Keys of Identity tables are mapped in OnModelCreating method of IdentityDbContext class
+            base.OnModelCreating(modelBuilder);            
+        }
+    }
+}
