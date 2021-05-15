b0618efa411390d6acc9da7825f09b58fa446e15
Rachit Srivastava <rachitsrivastava77@gmail.com>
2021-05-15 16:22:50 +0530

    Edit a Role in Administrator

--- LearningCoreWeb/LearningCoreWeb/Controllers/AdministrationController.cs
+++ LearningCoreWeb/LearningCoreWeb/Controllers/AdministrationController.cs
@@ -11,10 +11,12 @@ namespace LearningCoreWeb.Controllers
     public class AdministrationController : Controller
     {
         private readonly RoleManager<IdentityRole> roleManager;
+        private readonly UserManager<IdentityUser> userManager;
 
-        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        public AdministrationController(RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager)
         {
             this.roleManager = roleManager;
+            this.userManager = userManager;
         }
 
         [HttpGet]
@@ -51,78 +53,78 @@ namespace LearningCoreWeb.Controllers
             return View(model);
         }
 
-        //[HttpGet]
-        //public IActionResult ListRoles()
-        //{
-        //    var roles = roleManager.Roles;
-        //    return View(roles);
-        //}
+        [HttpGet]
+        public IActionResult ListRoles()
+        {
+            var roles = roleManager.Roles;
+            return View(roles);
+        }
 
 
         //// Role ID is passed from the URL to the action
-        //[HttpGet]
-        //public async Task<IActionResult> EditRole(string id)
-        //{
-        //    // Find the role by Role ID
-        //    var role = await roleManager.FindByIdAsync(id);
+        [HttpGet]
+        public async Task<IActionResult> EditRole(string id)
+        {
+            // Find the role by Role ID
+            var role = await roleManager.FindByIdAsync(id);
 
-        //    if (role == null)
-        //    {
-        //        ViewBag.ErrorMessage = $"Role with Id = {id} cannot be found";
-        //        return View("NotFound");
-        //    }
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {id} cannot be found";
+                return View("NotFound");
+            }
 
-        //    var model = new EditRoleViewModel
-        //    {
-        //        Id = role.Id,
-        //        RoleName = role.Name
-        //    };
+            var model = new EditRoleViewModel
+            {
+                Id = role.Id,
+                RoleName = role.Name
+            };
 
-        //    // Retrieve all the Users
-        //    //foreach (var user in userManager.Users)
-        //    //{
-        //    //    // If the user is in this role, add the username to
-        //    //    // Users property of EditRoleViewModel. This model
-        //    //    // object is then passed to the view for display
-        //    //    if (await userManager.IsInRoleAsync(user, role.Name))
-        //    //    {
-        //    //        model.Users.Add(user.UserName);
-        //    //    }
-        //    //}
+            // Retrieve all the Users
+            foreach (var user in userManager.Users.ToList())
+            {
+                // If the user is in this role that we got on line 69, add the username to
+                // Users property of EditRoleViewModel. This model
+                // object is then passed to the view for display
+                if (await userManager.IsInRoleAsync(user, role.Name))
+                {
+                    model.Users.Add(user.UserName);
+                }
+            }
 
-        //    return View(model);
-        //}
+            return View(model);
+        }
 
         //// This action responds to HttpPost and receives EditRoleViewModel
-        //[HttpPost]
-        //public async Task<IActionResult> EditRole(EditRoleViewModel model)
-        //{
-        //    var role = await roleManager.FindByIdAsync(model.Id);
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditRoleViewModel model)
+        {
+            var role = await roleManager.FindByIdAsync(model.Id);
 
-        //    if (role == null)
-        //    {
-        //        ViewBag.ErrorMessage = $"Role with Id = {model.Id} cannot be found";
-        //        return View("NotFound");
-        //    }
-        //    else
-        //    {
-        //        role.Name = model.RoleName;
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {model.Id} cannot be found";
+                return View("NotFound");
+            }
+            else
+            {
+                role.Name = model.RoleName;
 
-        //        // Update the Role using UpdateAsync
-        //        var result = await roleManager.UpdateAsync(role);
+                // Update the Role using UpdateAsync
+                var result = await roleManager.UpdateAsync(role);
 
-        //        if (result.Succeeded)
-        //        {
-        //            return RedirectToAction("ListRoles");
-        //        }
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListRoles");
+                }
 
-        //        foreach (var error in result.Errors)
-        //        {
-        //            ModelState.AddModelError("", error.Description);
-        //        }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
 
-        //        return View(model);
-        //    }
-        //}
+                return View(model);
+            }
+        }
     }
 }
